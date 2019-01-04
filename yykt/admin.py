007f736b88a1551c1888c65012921711fb595f96
lizhi <lizhi1026@126.com>
2019-01-04 21:29:27 +0800

    add course category

--- yykt/admin.py
+++ yykt/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import *
 
 # Register your models here.
 
@@ -11,4 +12,10 @@ admin.site.index_title = '星星课堂'
 admin.site.site_url = 'www.baidu.com'
 
 admin.site.disable_action('delete_selected')
+
+class CourseCategotyAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(CourseCategoty,CourseCategotyAdmin)
 
--- yykt/models.py
+++ yykt/models.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class CourseCategoty(models.Model):
+    name = models.CharField('类别', max_length=16, unique=True, error_messages={'unique':'这个分类已存在'})
+    createTime = models.DateTimeField('创建时间',auto_now_add=True)
+    updateTime = models.DateTimeField('更新时间', auto_now=True)
+    
+
+    def __str__(self):
+        return self.name
+    
+    def __unicode__(self):
+        return self.name
+    
+    def toDict(self):
+        return {'name': self.name}
+    
+    class Meta:
+        verbose_name_plural = '类别'
+        verbose_name = '类别'
