6a111a34e9a3970eda32a7d9c9b87bfc3ef21697
lizhi <lizhi1026@126.com>
2019-01-06 14:05:05 +0800

    add course model

--- yykt/models.py
+++ yykt/models.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+COURSE_FORMAT_CHOICES = (
+    (0, u'视频'),
+    (1, u'音频'),
+    (3, u'文章'),
+)
+
+
 class CourseCategoty(models.Model):
     name = models.CharField('类别', max_length=16, unique=True, error_messages={'unique':'这个分类已存在'})
     createTime = models.DateTimeField('创建时间',auto_now_add=True)
@@ -41,4 +48,20 @@ class Tag(models.Model):
         verbose_name = '标签'
         verbose_name_plural = verbose_name
         db_table = 'category'
-        
+
+class Course(models.Model):
+    name = models.CharField('课程名称',max_length=32, unique=True, error_messages={'unique':'这个课程已存在'})
+    type= models.IntegerField('类型',choices=COURSE_FORMAT_CHOICES,max_length=1,default=0)
+    introdcution = models.CharField('课程介绍',max_length=128,blank=True,null=True)
+    originPrice = models.IntegerField('原价',default=0)
+    price = models.IntegerField('价格',default=0)
+    category = models.ForeignKey(CourseCategoty,verbose_name='课程类别')
+    filePath = models.ImageField(upload_to='upload',verbose_name='封面上传')
+    tag = models.ManyToManyField(Tag,verbose_name='标签')
+    likes = models.IntegerField('喜欢',default=0,editable=False)
+    collect = models.IntegerField('收藏',default=0,editable=False)
+    createTime = models.DateTimeField('创建时间',auto_now_add=True)
+    updateTime = models.DateTimeField('更新时间', auto_now=True)
+    
+
+
